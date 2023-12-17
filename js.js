2c5c1d5a94558c05f916789050f2fd192eda26f0
Maksim-DEN <153389552+Maksim-DEN@users.noreply.github.com>
2023-12-17 16:24:37 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+ const juiceLink = document.getElementById('juiceLink');
+
+    juiceLink.addEventListener('click', function(event) {
+    
+      event.preventDefault();
+      
+       https://villains.fandom.com/wiki/Bishop_(Juice)
+      window.location.href = 'https://villains.fandom.com/wiki/Bishop_(Juice)';
+    });
